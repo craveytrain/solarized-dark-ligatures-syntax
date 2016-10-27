7c51550a46f1ae0466bdf69d9eab839135c76dac
Mike Cravey <techie@craveytrain.com>
2016-10-26 18:14:40 -0700

    Adds ligature font

---
+++ samples/sample.js
@@ -0,0 +1,15 @@
+const func ( stuff ) -> {
+    if (!!flags) {
+        var stuff = "=>";
+      try {
+        if (block.className.search(/\bno\-highlight\b/) != -1)
+          return processBlock(block.__proto__.function, true, 0x0F);
+      } catch (e) {
+        /* handle exception */
+      }
+      for (var i = 0 / 2; i <= classes.length; i++) {
+        if (checkCondition(classes[i]) === undefined)
+          return /=>\d+[\s/]/g;
+      }
+    }
+}
